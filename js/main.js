bb5f41d9aabb495165429ec81464aed12b7185cf
dima <dmitriyandronov@MacBook-Pro-Dmitriy.local>
2020-01-31 23:40:24 +0300

    Личный проект часть 2

--- js/main.js
+++ js/main.js
@@ -4,14 +4,18 @@ var NUMBER_OF_ADS = 8;
 var BOOKING_TYPES = ['flat', 'house', 'bungalo', 'palace'];
 var BOOKING_CHECKINS = ['12:00', '13:00', '14:00'];
 var BOOKING_CHECKOUTS = ['12:00', '13:00', '14:00'];
+var BOOKING_TITLES = ['Уютное гнездышко для молодоженов', 'Апартаменты в центре города', 'Дом с видом на горы', 'Дворец 16 века'];
 var BOOKING_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var BOOKING_DESCRIPTION = ['прекрасный вид на море', 'бассейн', 'все включено', 'гости оценили завтраки', 'близко к морю', 'семейный отдых', 'выбор клиентов', 'лучшая цена', 'ранее бронирование'];
+var BOOKING_DESCRIPTION = [
+  'Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.',
+  'Развлечения и бесплатные услуги в отелях citizenM Hotels.',
+  'Музыкальный плейлист от группы отелей Morgans Hotel Group. Для меломанов всех возрастов.',
+  'Hilton Suggests осуществляет коммуникации только посредством Twitter, чтобы помочь людям, планирующим поездку в различные направления, сделать их путешествие легче и приятнее.'
+];
 var BOOKING_PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel4.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel5.jpg'
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
@@ -37,10 +41,16 @@ var PIN_COORDS = {
     max: 630
   }
 };
+var ACCOMMODATION_TYPE = {
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalo: 'Бунгало',
+  palace: 'Дворец'
+};
+
 var map = document.querySelector('.map');
 var mapPin = document.querySelector('.map__pins');
-// var popupTemplate = document.querySelector('#card').content.querySelector('.map__card');
-// var popupElement = popupTemplate.cloneNode(true);
+var mapFilters = map.querySelector('.map__filters-container');
 
 var getRandomNumberInRange = function (min, max) {
   return Math.floor(min + Math.random() * (max - min));
@@ -77,7 +87,7 @@ var drawPage = function () {
   removeClass(map, 'map--faded');
 };
 
-var generateAd = function (authorNumber) {
+var createObject = function (authorNumber) {
   var cordsX = getRandomNumberInRange(PIN_COORDS.xCord.min, PIN_COORDS.xCord.max);
   var cordsY = getRandomNumberInRange(PIN_COORDS.yCord.min, PIN_COORDS.yCord.max);
   return {
@@ -85,7 +95,7 @@ var generateAd = function (authorNumber) {
       avatar: 'img/avatars/user0' + authorNumber + '.png'
     },
     offer: {
-      title: '',
+      title: getRandomItem(BOOKING_TITLES),
       address: cordsX + ', ' + cordsY,
       price: getRandomNumberInRange(BOOKING_PRICE.min, BOOKING_PRICE.max),
       type: getRandomItem(BOOKING_TYPES),
@@ -94,7 +104,7 @@ var generateAd = function (authorNumber) {
       checkin: getRandomItem(BOOKING_CHECKINS),
       checkout: getRandomItem(BOOKING_CHECKOUTS),
       features: getArrayWithRandomLength(BOOKING_FEATURES),
-      description: getArrayWithRandomLength(BOOKING_DESCRIPTION),
+      description: getRandomItem(BOOKING_DESCRIPTION),
       photos: getArrayWithRandomLength(BOOKING_PHOTOS)
     },
     location: {
@@ -104,10 +114,24 @@ var generateAd = function (authorNumber) {
   };
 };
 
+var generateLabelledList = function (array) {
+  var cardList = array.map(function (item) {
+    return '<li class="popup__feature popup__feature--' + item + '"></li>';
+  }).join('\n');
+  return cardList;
+};
+
+var generateGalleryElements = function (array) {
+  var cardGallery = array.map(function (item) {
+    return '<img src="' + item + '"' + ' class="popup__photo" width="45" height="40" alt="Фотография жилья">';
+  }).join('\n');
+  return cardGallery;
+};
+
 var generateAds = function (numberOfElements) {
   var generatedItems = [];
   for (var i = 0; i < numberOfElements; i++) {
-    generatedItems.push(generateAd(i + 1));
+    generatedItems.push(createObject(i + 1));
   }
   return generatedItems;
 };
@@ -121,14 +145,40 @@ var generatePin = function (ad) {
   return pinElement;
 };
 
-var renderPins = function (listOfItems) {
+var fillCard = function (cardItem) {
+  var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+  var cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__title').textContent = cardItem.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = cardItem.offer.address;
+  cardElement.querySelector('.popup__text--price').innerHTML = cardItem.offer.price + ' &#x20bd;/ночь';
+  cardElement.querySelector('.popup__type').textContent = ACCOMMODATION_TYPE[cardItem.offer.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = cardItem.offer.rooms + ' комнаты для ' + cardItem.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + cardItem.offer.checkin + ', выезд до ' + cardItem.offer.checkout;
+  cardElement.querySelector('.popup__features').innerHTML = '';
+  cardElement.querySelector('.popup__features').insertAdjacentHTML('beforeend', generateLabelledList(cardItem.offer.features));
+  cardElement.querySelector('.popup__description').textContent = cardItem.offer.description;
+  cardElement.querySelector('.popup__photos').innerHTML = '';
+  cardElement.querySelector('.popup__photos').insertAdjacentHTML('beforeend', generateGalleryElements(cardItem.offer.photos));
+  return cardElement;
+};
+
+var renderPins = function (elements) {
   var fragment = document.createDocumentFragment();
-  listOfItems.forEach(function (ad) {
+  elements.forEach(function (ad) {
     fragment.appendChild(generatePin(ad));
   });
   mapPin.appendChild(fragment);
 };
 
+var renderCards = function (elements) {
+  var fragment = document.createDocumentFragment();
+  elements.forEach(function (card) {
+    fragment.appendChild(fillCard(card));
+  });
+  mapFilters.appendChild(fragment);
+};
+
 var adsList = generateAds(NUMBER_OF_ADS);
 drawPage();
 renderPins(adsList);
+renderCards(adsList);

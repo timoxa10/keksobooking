97a17fcf87bbb174fc1f961e5d1df2a908abe55f
dima <dmitriyandronov@MacBook-Pro-Dmitriy.local>
2020-02-14 18:41:42 +0300

    Изменение модуля диалога

--- js/dialog-util.js
+++ js/dialog-util.js
@@ -5,15 +5,21 @@
   var ESC_KEY = 'Escape';
   var LEFT_MOUSE_BUTTON_KEY = 0;
 
+  var isEnterPressed = function (evt) {
+    return evt.key === ENTER_KEY;
+  };
+
+  var isEscPressed = function (evt) {
+    return evt.key === ESC_KEY;
+  };
+
+  var isLeftPressed = function (evt) {
+    return evt.button === LEFT_MOUSE_BUTTON_KEY;
+  };
+
   window.dialogUtil = {
-    isEnterPressed: function (evt) {
-      return evt.key === ENTER_KEY;
-    },
-    isEscPressed: function (evt) {
-      return evt.key === ESC_KEY;
-    },
-    isLeftPressed: function (evt) {
-      return evt.button === LEFT_MOUSE_BUTTON_KEY;
-    }
+    isEnterPressed: isEnterPressed,
+    isEscPressed: isEscPressed,
+    isLeftPressed: isLeftPressed
   };
 })();

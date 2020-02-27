71481d91784d063799d4372094b65c0710e8b6eb
dima <dmitriyandronov@MacBook-Pro-Dmitriy.local>
2020-02-27 22:14:37 +0300

    Фильтрация данных

--- js/filter.js
+++ js/filter.js
@@ -7,20 +7,68 @@
   var removePins = window.pin.remove;
 
   var mapFiltersFormChangeHandler = function () {
-    updatePins();
+    window.debounce.call(updatePins());
   };
 
   var updatePins = function () {
     window.card.close();
     removePins(window.mapPins);
+
     var housingType = document.querySelector('#housing-type').value;
-    renderPins(window.ads.filter(function (ad) {
-      if (housingType === 'any') {
-        return true;
+    var housingPrice = document.querySelector('#housing-price').value;
+    var housingRooms = document.querySelector('#housing-rooms').value;
+    var housingGuests = document.querySelector('#housing-guests').value;
+    var housingFeatures = document.querySelectorAll('#housing-features input');
+    var checkedValue = null;
+    for (var i = 0; i < housingFeatures.length; i++) {
+      if (housingFeatures[i].checked) {
+        checkedValue = housingFeatures[i].value;
       }
-      return ad.offer.type === housingType;
-    }));
+    }
+
+    renderPins(window.ads
+      .filter(function (ad) {
+        if (housingType === 'any') {
+          return true;
+        }
+        return ad.offer.type === housingType;
+      })
+      .filter(function (ad) {
+        switch (housingPrice) {
+          case 'any':
+            return true;
+          case 'middle':
+            return ad.offer.price >= 10000 && ad.offer.price <= 50000;
+          case 'low':
+            return ad.offer.price <= 9999;
+          default:
+            return ad.offer.price >= 50001;
+        }
+      })
+      .filter(function (ad) {
+        if (housingRooms === 'any') {
+          return true;
+        }
+        return ad.offer.rooms === Number(housingRooms);
+      })
+      .filter(function (ad) {
+        if (housingGuests === 'any') {
+          return true;
+        }
+        return ad.offer.guests === Number(housingGuests);
+      })
+      .filter(function (ad) {
+        if (!checkedValue) {
+          return true;
+        }
+        return ad.offer.features.includes(checkedValue);
+      })
+    );
   };
 
   mapFiltersForm.addEventListener('change', mapFiltersFormChangeHandler);
+
+  window.filter = {
+    update: updatePins
+  };
 })();

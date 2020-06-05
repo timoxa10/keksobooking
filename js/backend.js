696cbf52b8256a38166338e505d002e97251f494
dima <timoxa9510@gmail.com>
2020-06-05 12:38:03 +0300

    Change server url

--- js/backend.js
+++ js/backend.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var URL = 'https://js.dump.academy/keksobooking';
+  var URL = 'https://javascript.pages.academy/keksobooking';
   var TIMEOUT_IN_MS = 10000;
   var StatusCode = {
     OK: 200
